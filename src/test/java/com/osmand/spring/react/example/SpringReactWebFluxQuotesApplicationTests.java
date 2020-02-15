fe071c39cff8d898d69b58fa69d5b8d52e48d11a
Osmand Das <osmandpdas@gmail.com>
2020-02-15 01:12:24 -0500

    added test case

--- src/test/java/com/osmand/spring/react/example/SpringReactWebFluxQuotesApplicationTests.java
+++ src/test/java/com/osmand/spring/react/example/SpringReactWebFluxQuotesApplicationTests.java
@@ -1,5 +1,9 @@
 package com.osmand.spring.react.example;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.concurrent.CountDownLatch;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -9,8 +13,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 import com.osmand.spring.react.example.domain.Quote;
 
@@ -47,6 +49,34 @@ public class SpringReactWebFluxQuotesApplicationTests {
 			assertThat(	allQuotes.getResponseBody())
 						.allSatisfy(quote -> assertThat(quote.getPrice()).isPositive());
 		});
+		
+	}
+	
+	@Test
+	public void testStreamQuotes() throws Exception {
+		
+		logger.info("\n\n in testStreamQuotes .......");
+		
+		int size = 10;
+		
+		CountDownLatch countDownLatch = new CountDownLatch(size);
+		
+		webTestClient
+		.get()
+		.uri("/quotes?size=20")
+		.accept(MediaType.APPLICATION_STREAM_JSON)
+		.exchange()
+		.returnResult(Quote.class)
+		.getResponseBody()
+		.take(size)
+		.subscribe(quote -> {
+			assertThat(	quote.getPrice()).isPositive();
+			countDownLatch.countDown();
+		});
+		
+		countDownLatch.await();
+		
+		logger.info("Test complete...");
 		
 	}
 	
